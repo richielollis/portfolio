0edb3c59bec0c219afba589802394b2b6bc5139c
richielollis <richiedlollis@gmail.com>
2023-02-01 22:15:40 -0500

    responsive design implemented for multiple screen sizes

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -5,12 +5,14 @@ function Work() {
     {
       name: "ARIJ",
       description: "A task/ticket managment website",
+      github: "https://github.com/Jstelzl/arij",
       live: "https://pacific-cliffs-88620.herokuapp.com/",
       picture: require(`../../assets/images/Arij-logo.png`),
     },
     {
       name: "POKENECT",
       description: "A social media website for fans of Pokemon to connect",
+      github: "https://github.com/PDismore/PokeNect",
       live: "https://dry-wildwood-44635.herokuapp.com/",
       picture: require(`../../assets/images/PokeNect2.png`),
     },
@@ -18,25 +20,33 @@ function Work() {
       name: "LAUNCH",
       description:
         "Fun app that shows which SpaceX rocket you'll take to Mars and how many asteroids you'll encounter",
+      github: "https://github.com/richielollis/launch",
       live: "https://richielollis.github.io/launch/",
       picture: require(`../../assets/images/Launch.jpg`),
     },
     {
       name: "JATE",
       description: "PWA text editor",
+      github: "https://github.com/richielollis/just-another-text-editor",
       live: "https://limitless-earth-25385.herokuapp.com/",
       picture: require(`../../assets/images/JATE.png`),
     },
   ]);
   return (
-    <div>
+    <div id="projectDiv">
       {works.map((work) => (
         <div className="project">
           <div className="projectText">
             <a href={work.live} target="_blank" rel="noreferrer">
               <h2>{work.name}</h2>
             </a>
-            <p id="projectDescription">{work.description}</p>
+            <p id="projectDescription">
+              {work.description} <br></br>
+              <br></br>
+              <a id="githubLink" href={work.github} target="_blank">
+                | GitHub |
+              </a>
+            </p>
           </div>
           <a
             className="projectImg"
